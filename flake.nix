dbf48bb900bd2adf3c493326616b8b3432d1e450
Mearkatz <drquinsey@gmail.com>
2024-11-24 10:53:16 -0500

    removed flake.nix

--- flake.nix
+++
@@ -1,7 +0,0 @@
-{
-  description = "A basic flake for my rust projects";
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-  };
-}
